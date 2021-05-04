299f04ba2e2a3725d083b0d4249b55e17a5250cc
nurlanulyxv <81552345+nurlanulyxv@users.noreply.github.com>
2021-05-04 11:20:56 +0600

    Update Final.py

--- Final.py
+++ Final.py
@@ -1,12 +1,10 @@
-import numpy 
 import streamlit as st
 import pandas as pd
 import base64
-import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
+
 
 st.title('Project by Yerkebulan and Alibek')
+
 
 st.markdown("""
 This app performs simple webscraping of NFL Football player stats data (focusing on Rushing)
