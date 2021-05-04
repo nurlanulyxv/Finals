a921ca132f21dc58345672234824fa80464ae6a9
nurlanulyxv <81552345+nurlanulyxv@users.noreply.github.com>
2021-05-04 10:30:25 +0600

    Update Final.py

--- Final.py
+++ Final.py
@@ -16,6 +16,7 @@ st.sidebar.header('User Input Features')
 selected_year = st.sidebar.selectbox('Year', list(reversed(range(1990,2020))))
 
 @st.cache
+
 def load_data(year):
     url = "https://www.pro-football-reference.com/years/" + str(year) + "/rushing.htm"
     html = pd.read_html(url, header = 1)
@@ -25,9 +26,11 @@ def load_data(year):
     playerstats = raw.drop(['Rk'], axis=1)
     return playerstats
 playerstats = load_data(selected_year)
-
+  
+    
 team = sorted(playerstats.Tm.unique())
 selected_team = st.sidebar.multiselect('Team', team, team)
+
 
 position = ['RB','QB','WR','FB','TE']
 selected_position = st.sidebar.multiselect('Position', position, position)
